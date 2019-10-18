2678511f50b2bb23eaa495b5de8aced5c4a56d74
MinexAutomation <minex.automation@gmail.com>
2019-10-18 14:21:31 -0700

    Added what seem to be basic extensions to the JsonSerializer type.

---
+++ source/R5T.Jutland.Newtonsoft/Code/Extensions/JsonSerializerExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+using Newtonsoft.Json;
+
+
+namespace R5T.Jutland.Newtonsoft
+{
+    public static class JsonSerializerExtensions
+    {
+        public static void Serialize<T>(this JsonSerializer jsonSerializer, string jsonFilePath, T value)
+        {
+            using (var textFileWriter = File.CreateText(jsonFilePath))
+            {
+                jsonSerializer.Serialize(textFileWriter, value);
+            }
+        }
+
+        public static T Deserialize<T>(this JsonSerializer jsonSerializer, string jsonFilePath)
+        {
+            using (var textReader = File.OpenText(jsonFilePath))
+            using (var jsonReader = new JsonTextReader(textReader))
+            {
+                var value = jsonSerializer.Deserialize<T>(jsonReader);
+                return value;
+            }
+        }
+    }
+}
